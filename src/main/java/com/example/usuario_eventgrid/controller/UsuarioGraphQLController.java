6589dcf7170a41a1be6d0241d91f4dac1793f904
Eduardo Carvajal <edo.carvajal@hotmail.com>
2025-05-06 19:36:52 -0400

    chore:  se agrega metodo de eliminar y actualizar con graphql

--- src/main/java/com/example/usuario_eventgrid/controller/UsuarioGraphQLController.java
+++ src/main/java/com/example/usuario_eventgrid/controller/UsuarioGraphQLController.java
@@ -6,6 +6,7 @@ import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
 
 import com.example.usuario_eventgrid.DTO.UsuarioCreateRequest;
+import com.example.usuario_eventgrid.DTO.UsuarioUpdateRequest;
 import com.example.usuario_eventgrid.service.EventGridPublisherService;
 
 @Controller
@@ -19,4 +20,17 @@ public class UsuarioGraphQLController {
         publisherService.publishEvent("UsuarioCreado", "/usuario", input);
         return "Usuario creado.";
     }
+
+    @MutationMapping
+    public String updateUsuario(@Argument String id, @Argument("input") UsuarioUpdateRequest input) {
+        publisherService.publishEvent("UsuarioActualizado", "/usuario/" + id, input);
+        return "Usuario actualizado.";
+    }
+
+    @MutationMapping
+    public String deleteUsuario(@Argument String id, @Argument("input") String entity) {
+        publisherService.publishEvent("UsuarioEliminado", "/usuario/" + id, entity);
+        return "Usuario eliminado.";
+    }
+
 }
